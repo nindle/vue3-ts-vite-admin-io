5714cc853f1d0b03c97bf03ed1e5dcd117c3440f
liuchao <1104892323@qq.com>
2022-04-12 21:58:24 +0800

    登录页面

--- components.d.ts
+++ components.d.ts
@@ -4,6 +4,12 @@
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AButton: typeof import('ant-design-vue/es')['Button']
+    ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
+    AForm: typeof import('ant-design-vue/es')['Form']
+    AFormItem: typeof import('ant-design-vue/es')['FormItem']
+    AInput: typeof import('ant-design-vue/es')['Input']
+    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
     ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,85 @@
 <template>
-  <div class="css" @click="goAbout">登录</div>
+  <div class="login">
+    <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 6 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+    >
+      <a-form-item
+        label="账号"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+      >
+        <a-input v-model:value="formState.username" />
+      </a-form-item>
+
+      <a-form-item
+        label="密码"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+      >
+        <a-input-password v-model:value="formState.password" />
+      </a-form-item>
+
+      <a-form-item name="remember" :wrapper-col="{ offset: 6, span: 16 }">
+        <a-checkbox v-model:checked="formState.remember">记住密码</a-checkbox>
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 6, span: 16 }">
+        <a-button type="primary" html-type="submit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import store from '../../store/index';
+import { ref } from "vue";
+import { defineComponent, reactive } from "vue";
+import { useRouter } from "vue-router";
+import store from "../../store/index";
+
 const router = useRouter();
+
 const goAbout = () => {
-  store.commit('setToken');
-  router.push({ path: '/' });
+  store.commit("setToken");
+  router.push({ path: "/" });
+};
+
+interface FormState {
+  username: string;
+  password: string;
+  remember: boolean;
+}
+
+const formState = reactive<FormState>({
+  username: "",
+  password: "",
+  remember: true,
+});
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.login {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  .ant-form {
+    width: 600px;
+    height: 300px;
+    background-color: #ccc;
+    margin: 200px auto;
+  }
+}
+</style>
