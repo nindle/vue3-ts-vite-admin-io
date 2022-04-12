1e340599254706c0a43131d75df964dfd2307212
liuchao <1104892323@qq.com>
2022-04-12 14:19:14 +0800

    整理文件

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/about/index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/index.vue')
+  },
+]
+
+const router = createRouter({
+  // history: createWebHashHistory(),
+  history: createWebHistory("/"),
+  routes,
+})
+
+export default router
