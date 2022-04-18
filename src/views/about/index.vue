0face7c04f904a9433d2325dee7c03dae75f8b01
liuchao <1104892323@qq.com>
2022-04-18 21:28:48 +0800

    菜单

---
+++ src/views/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style scoped lang="less"></style>
