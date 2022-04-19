d91ac0a6e120c24b9a726e24090153c72a214ca6
liuchao <1104892323@qq.com>
2022-04-19 16:01:07 +0800

    增加调试

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="">123</div>
+  <div class="" @click="lianjie">123</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+const lianjie = async () => {};
 </script>
 
 <style scoped lang="scss"></style>
